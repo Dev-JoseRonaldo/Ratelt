79eca7865e64373ee445f2ed813c5db188781e21
José Ronaldo <dev.joseronaldo@gmail.com>
2024-08-24 21:09:25 -0300

    style: add colors in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss";
 
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,10 +10,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        xxs: '320px',
+        xs: '400px',
+        ...defaultTheme .screens,
+      },
+      colors: {
+        primary: '#1F64FF',
+        red: '#EA5050',
+        yellow: '#FFD130',
+        orange: '#FFB36C',
+        blue: {
+          400: '#1A3EC1',
+          500: '#072AC8',
+          900: '#000022',
+        },
+        green: {
+          300: '#30F2F2',
+          400: '#73FBD3',
+          500: '#15D6B3',
+          600: '#35D5C3',
+        }
       },
     },
   },
