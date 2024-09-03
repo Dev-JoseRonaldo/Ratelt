c9a6a049c293e39eb7492b7da5bf03033ce62a9f
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-09-02 23:58:26 -0300

    feat: get backend data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+'use client';
+
+import { AxiosInterceptor } from '@/interceptor/axios.interceptor';
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+  const axiosInstance = new AxiosInterceptor();
+  const axios = axiosInstance.getAxiosInstance();
+  const [title, setTitle] = useState<string>();
+  useEffect(() => {
+    axios.get(`${process.env.NEXT_PUBLIC_API}/health`).then((r) => {
+      setTitle(r.data.status);
+    });
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-5xl font-bold">Rate It</h1>
+      <h1 className="text-5xl font-bold">{title}</h1>
     </main>
   );
 }
